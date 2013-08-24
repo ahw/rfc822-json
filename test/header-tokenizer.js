03613fcb065fe9b8facbe55271c32c888f61d7a3
ahw <andrewhallagan@gmail.com>
2013-08-24 15:34:32 -0700

    More unit tests.

--- test/header-tokenizer.js
+++ test/header-tokenizer.js
@@ -1,4 +1,3 @@
-var Request = require('request');
 var Sinon = require('sinon');
 var FileSystem = require('fs');
 var HeaderTokenizer = require(__dirname + '/../lib/header-tokenizer');
@@ -46,6 +45,76 @@ module.exports = {
         this.tokenizer.write(fieldName + ':' + fieldBody + '\r\n');
         this.tokenizer.write(fieldName + ':' + fieldBody + '\r\n');
         this.tokenizer.write(fieldName + ':' + fieldBody + '\r\n');
+        this.tokenizer.end('\r\n\r\n');
+    },
+
+    'Collapses simple internal whitespace': function(test) {
+        var fieldName = 'Content-Type';
+        var fieldBody = 'foo     bar';
+
+        var wrapper = {tokenHandler: function() {} };
+        var spy = Sinon.spy(wrapper, 'tokenHandler');
+
+        this.tokenizer.on('token', spy);
+        this.tokenizer.on('done', function() {
+            test.ok(spy.secondCall.calledWith({type: 'field_body', value: 'foo bar'}));
+            test.done();
+        });
+
+        this.tokenizer.write(fieldName + ':' + fieldBody);
+        this.tokenizer.end('\r\n\r\n');
+    },
+
+    'Handles field body folding': function(test) {
+        var fieldName = 'Content-Type';
+        var fieldBody = 'foo\r\n\tbar\r\n    baz';
+
+        var wrapper = {tokenHandler: function() {} };
+        var spy = Sinon.spy(wrapper, 'tokenHandler');
+
+        this.tokenizer.on('token', spy);
+        this.tokenizer.on('done', function() {
+            test.ok(spy.secondCall.calledWith({type: 'field_body', value: 'foo bar baz'}));
+            test.equal(spy.callCount, 2);
+            test.done();
+        });
+
+        this.tokenizer.write(fieldName + ':' + fieldBody);
+        this.tokenizer.end('\r\n\r\n');
+    },
+
+    'Collapses complex internal whitespace (CR, LF chars)': function(test) {
+        var fieldName = 'Content-Type';
+        var fieldBody = 'one\rtwo\nthree\r four\n five\r\tsix\n\tseven\r\reight\n\nnine';
+
+        var wrapper = {tokenHandler: function() {} };
+        var spy = Sinon.spy(wrapper, 'tokenHandler');
+
+        this.tokenizer.on('token', spy);
+        this.tokenizer.on('done', function() {
+            test.ok(spy.secondCall.calledWith({type: 'field_body', value: 'one two three four five six seven eight nine'}));
+            test.equal(spy.callCount, 2);
+            test.done();
+        });
+
+        this.tokenizer.write(fieldName + ':' + fieldBody);
+        this.tokenizer.end('\r\n\r\n');
+    },
+
+    'Trims outside whitespace in field body': function(test) {
+        var fieldName = 'Content-Type';
+        var fieldBody = '    foo     ';
+
+        var wrapper = {tokenHandler: console.log };
+        var spy = Sinon.spy(wrapper, 'tokenHandler');
+
+        this.tokenizer.on('token', spy);
+        this.tokenizer.on('done', function() {
+            test.ok(spy.secondCall.calledWith({type: 'field_body', value: 'foo'}));
+            test.done();
+        });
+
+        this.tokenizer.write(fieldName + ':' + fieldBody);
         this.tokenizer.end('\r\n\r\n');
     }
 
