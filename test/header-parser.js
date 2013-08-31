d2ad2cf285b92317b6b6d622fccd95500e36daaf
ahw <andrewhallagan@gmail.com>
2013-08-31 14:49:49 -0700

    HeaderParser is a Transform stream.
    Pushes JSON string of headers object. TODO: make this an objectStream?

--- test/header-parser.js
+++ test/header-parser.js
@@ -1,12 +1,14 @@
-// -- var Request = require('request');
-// -- var FileSystem = require('fs');
-// -- var HeaderParser = require(__dirname + '/../lib/header-parser');
-// -- var util = require('util');
-// -- 
-// -- var testEmail = FileSystem.createReadStream(__dirname + '/email1.txt');
-// -- var parser = new HeaderParser();
-// -- testEmail.pipe(parser);
-// -- 
-// -- parser.on('headers', function(headers) {
-// --     console.log(util.inspect(headers, {colors: true}));
-// -- });
+var FileSystem = require('fs');
+var HeaderParser = require(__dirname + '/../lib/header-parser');
+var util = require('util');
+
+var testEmail = FileSystem.createReadStream(__dirname + '/email1.txt');
+var parser = new HeaderParser();
+testEmail.pipe(parser);
+
+// This works
+// parser.on('headers', function(headers) {
+//     console.log(util.inspect(headers, {colors: true}));
+// });
+
+parser.pipe(process.stdout);
