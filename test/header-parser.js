9546957e2821e3b2f37038094f85351fd01ddd86
ahw <andrewhallagan@gmail.com>
2013-09-02 10:56:23 -0700

    In the process of refactoring.

--- test/header-parser.js
+++ test/header-parser.js
@@ -1,14 +1,28 @@
+var Sinon = require('sinon');
 var FileSystem = require('fs');
 var HeaderParser = require(__dirname + '/../lib/header-parser');
 var util = require('util');
 
-var testEmail = FileSystem.createReadStream(__dirname + '/email1.txt');
-var parser = new HeaderParser();
-testEmail.pipe(parser);
+module.exports = {
+    setUp: function(callback) {
+        this.parser = new HeaderParser();
+        callback();
+    },
 
-// This works
-// parser.on('headers', function(headers) {
-//     console.log(util.inspect(headers, {colors: true}));
-// });
+    'Emits parsed headers object': function(test) {
+        testEmail = '';
+        testEmail += 'foo:foo\r\n';
+        testEmail += 'bar:bar\r\n';
+        testEmail += '\r\n';
 
-parser.pipe(process.stdout);
+        this.parser.on('headers', function(headers) {
+            test.ok(headers instanceof Object);
+            test.equal(headers.foo, 'foo');
+            test.equal(headers.bar, 'bar');
+            test.done();
+        });
+
+        this.parser.write(testEmail);
+    }
+};
+
